d5ffc5092d687a53280b93a4e7e2b7fb8e749b95
Kateryna <>
2017-02-11 12:56:10 +0200

    Naive bayse classifier + question-answer system

---
+++ NBC+QA/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace NBC_QA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NBCClass nbcc = new NBCClass();
+            QA qac = new QA();
+            WordStemming ws = new WordStemming();
+            SS_LDFS ssldfs = new SS_LDFS();
+
+            string str = "отели возле моря";
+            Console.WriteLine(str);
+            str = ws.getQuestion(str);
+            Console.WriteLine(str);
+            string [] fn = nbcc.nbcbuild_returnfn("person.txt", "location.txt", "date.txt", str);
+            Console.WriteLine(); 
+            string answer = qac.answer(fn, str);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NBC+QA/QA.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace NBC_QA
+{
+    class QA
+    {
+        SortedDictionary<string, string> ansdictionary = new SortedDictionary<string, string>();
+        WordStemming ws = new WordStemming();
+        
+        public string keystring (string s)
+        {
+            string ans = "";
+            int i = 0, j = s.Length;
+            while (i < j && s[i] != '/')
+            { ans += s[i]; i++; }
+            ans = ws.Stemword(ans);
+           return ans;
+        }
+        public static string getfilename(string s)
+        {
+            string ans = "";
+            int i = 0, j = s.Length;
+            while (i < j && s[i] != '.')
+            { ans += s[i]; i++; }
+            return ans;
+        }
+        public static string valuestring(string s)
+        {
+            string ans = "";
+            int i = 0, j = s.Length,k=1;
+            while (i < j && s[i] != '/')
+                i++;
+            k = i + 1;
+           while (k < j)
+            { ans += s[k]; k++; }
+            return ans;
+        }
+
+        public static string keywordindictionary(string s)
+        {
+            string ans = "";
+            int i = 0, j = s.Length, k = 1;
+            while (i < j && s[i] != '_')
+                i++;
+            k = i + 1;
+            while (k < j)
+            { ans += s[k]; k++; }
+            return ans;
+        }
+
+        public  void filetodic(SortedDictionary<string, string> ansdictionary,string filename)
+        {
+            string s = "";
+            string skey = "";
+            string svalue = "";
+            string fn = "answer" + filename;
+            string fninput = getfilename(filename);
+            StreamReader sr = new StreamReader(fn, Encoding.Default);
+            while (sr.EndOfStream != true)
+            {
+                s = sr.ReadLine() + '\n';
+                skey = fninput + '_' + keystring(s);
+                svalue = valuestring(s);
+                ansdictionary.Add(skey.Trim(),svalue.Trim());
+            }
+        }
+        public static string getkey(SortedDictionary<string, string> ansdictionary, string str)
+        {
+            string keystr = "",sss;
+            string[] words = str.Split(' ');
+            SortedDictionary<string, string>.KeyCollection keyColl = ansdictionary.Keys;
+            foreach (string ss in keyColl)
+            {
+                sss = keywordindictionary(ss.ToLower().Trim());
+                if (str.Contains(sss))
+                    keystr += ss + " ";
+            }
+            if (keystr.Length < 2) 
+            foreach (string ss in keyColl)
+            {
+                sss = keywordindictionary(ss.ToLower().Trim());
+                for (int i = 0; i < words.Length;i++)
+                if (sss.Contains(words[i]))
+                    keystr += ss + " ";
+            }
+            return keystr;
+        }
+
+
+        public static string getanswer(SortedDictionary<string, string> ansdictionary, string str)
+        {
+            string keystr = "",answer="";
+            keystr = getkey(ansdictionary, str);
+            ansdictionary.TryGetValue(keystr.Trim(), out answer);
+            return answer;
+        }
+
+        public string answer(string [] fn,string str)
+        {
+            string answer = "";
+            for (int i = 0; i < fn.Length; i++)
+            {
+                filetodic(ansdictionary, fn[i]);
+                answer += getanswer(ansdictionary, str) + "\n";
+                ansdictionary.Clear();
+            }
+            if (answer.Length < 2) answer = "Попробуйте задать вопрос по-другому (уточнив вопрос или перефразировать). Ответа на Ваш вопрос нет.";
+                return answer;
+        }
+    }
+}
